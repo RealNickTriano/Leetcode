3bc9e0f204a12a1a5ba3a4cec5c6bbd1befadc6d
Nicholas Triano <nicholastriano1999@gmail.com>
2023-10-13 18:42:32 -0400

    155 Min Stack

---
+++ Python/155_Min_Stack.py
@@ -0,0 +1,16 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        my_min = min(val, self.stack[-1][1] if self.stack else val)
+        self.stack.append((val, my_min))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
