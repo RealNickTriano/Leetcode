2a5fe6de7e5d0d6c4eb416651f126f8965ad8451
RealNickTriano <70707042+RealNickTriano@users.noreply.github.com>
2023-10-17 20:14:14 -0400

    125 Valid Palindrome

---
+++ Python/125_Valid_Palindrome.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # start and end pointer
+        # move towards center comparing letters
+        # skip non-alphanumerics
+        p1 = 0
+        p2 = len(s) - 1
+        while p1 < p2:
+            if not str.isalnum(s[p1]):
+                p1 += 1
+            elif not str.isalnum(s[p2]):
+                p2 -= 1
+            elif s[p1].lower() != s[p2].lower():
+                return False
+            else:
+                p1 += 1
+                p2 -= 1
+
+        return True
+
+sol = Solution()
+print(sol.isPalindrome("A man, a plan, a canal: Panama"))
+print(sol.isPalindrome("race a car"))
