229bfc052a9afb4ab28e701900af647dab89b2a0
RealNickTriano <nicholastriano1999@gmail.com>
2023-10-26 11:37:59 -0400

    704 Binary Search

--- Python/704_Binary_Search.py
+++ Python/704_Binary_Search.py
@@ -5,7 +5,8 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums) - 1
         while l <= r:
-            middle = r + l // 2
+            middle = (r + l) // 2
+            print(nums[l: r+1], middle)
             if nums[middle] > target:
                 r = middle - 1
             elif nums[middle] < target:
